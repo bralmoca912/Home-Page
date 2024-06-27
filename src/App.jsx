775b6dfffe6b67093c5e46d1fbe708154662e360
BrDev <bmontero912@gmail.com>
2024-06-27 15:18:30 -0500

    Agregado de assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Menu from "./shared/Menu";
+import Home from "./components/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="border text-3xl">Hola</div>
+      <div className="border text-3xl">
+        <Menu></Menu>
+        <Home></Home>
+      </div>
     </>
   );
 }
